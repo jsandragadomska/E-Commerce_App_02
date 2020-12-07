ba5072b72cd9c54b0bc298b829ed58780426b20e
jsandragadomska <nimuehh07@hmail.com>
2020-12-07 14:19:32 +0100

    new model - Post

--- store/models.py
+++ store/models.py
@@ -48,3 +48,13 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return self.address
+
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(blank=True, null=True)
+    snippet = models.CharField(max_length=225)
+    post_date = models.DateField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
